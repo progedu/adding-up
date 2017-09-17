ac6b38739762477d258e1e61de307416dff120af
whatalnk <watnk0806@gmail.com>
2017-09-17 14:56:39 +0000

    変化率を3桁に丸めた

--- app.js
+++ app.js
@@ -41,8 +41,58 @@ rl.on('close', () => {
     return pair2[1].change - pair1[1].change;
   });
   const rankingStrings = rankingArray.map((pair, i) => {
-    return (i + 1) + '. ' + pair[0] + ': ' + pair[1].popu10 + ' => ' + pair[1].popu15 + ' 変化率: ' + Math.round10(pair[1].change, -2);
+    return (i + 1) + '. ' + pair[0] + ': ' + pair[1].popu10 + ' => ' + pair[1].popu15 + ' 変化率: ' + Math.round10(pair[1].change, -3);
   })
   console.log(rankingStrings);
 });
 
+  /**
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+   * Decimal adjustment of a number.
+   * 
+   * * @param {String}  type  The type of adjustment.
+   * @param {Number}  value The number.
+   * @param {Integer} exp   The exponent (the 10 logarithm of the adjustment base).
+   * @returns {Number} The adjusted value.
+   */
+  function decimalAdjust(type, value, exp) {
+    // If the exp is undefined or zero...
+    if (typeof exp === 'undefined' || +exp === 0) {
+      return Math[type](value);
+    }
+    value = +value;
+    exp = +exp;
+    // If the value is not a number or the exp is not an integer...
+    if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+      return NaN;
+    }
+    // If the value is negative...
+    if (value < 0) {
+      return -decimalAdjust(type, -value, exp);
+    }
+    // Shift
+    value = value.toString().split('e');
+    value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+    // Shift back
+    value = value.toString().split('e');
+    return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+  }
+
+  // Decimal round
+  if (!Math.round10) {
+    Math.round10 = function(value, exp) {
+      return decimalAdjust('round', value, exp);
+    };
+  }
+  // Decimal floor
+  if (!Math.floor10) {
+    Math.floor10 = function(value, exp) {
+      return decimalAdjust('floor', value, exp);
+    };
+  }
+  // Decimal ceil
+  if (!Math.ceil10) {
+    Math.ceil10 = function(value, exp) {
+      return decimalAdjust('ceil', value, exp);
+    };
+  }
