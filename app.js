215cbb5e8387fd68ffc57b1f9c95da4f245b9acf
rinringo <rintarou_17n1101688@nnn.ed.jp>
2017-09-19 09:57:54 +0000

    ランキングの実装完了

--- app.js
+++ app.js
@@ -34,11 +34,12 @@ rl.on('close',() => {
         value.change = value.popu15 / value.popu10;
     }
     const rankingArray = Array.from(map).sort((pair1,pair2) => {
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change -  pair2[1].change;
+        
     });
 
-    const rankingSreing = rankingArray.map((pair) =>{
-        return pair[0] + ':' + pair[1].popu10 + '=>' + pair[1].popu15 + '変化率; ' + pair[1].change;
+    const rankingSreing = rankingArray.map((pair,i) =>{
+        return(i + 1) + '位 :' + pair[0] + ':' + pair[1].popu10 + '=>' + pair[1].popu15 + '変化率; ' + pair[1].change;
     });
     console.log(rankingSreing);
 });
