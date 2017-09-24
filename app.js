a996e7a1d7d2fe4f07e750a48222b488fb780847
yakuma <yakuma8@gmail.com>
2017-09-24 18:03:46 +0900

    降順にソート、出力にランキングを追加

--- app.js
+++ app.js
@@ -33,12 +33,12 @@ rl.on("close", () => {
         value.change = value.popu15 / value.popu10;
     }
     const rankingArray = Array.from(map).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
     })
-    const rankingString = rankingArray.map((pair) => {
-        return pair[0] + ":" + pair[1].popu10 + "=>" + pair[1].popu15 + " 変化率:" + pair[1].change;
+    const rankingString = rankingArray.map((pair, rank) => {
+        return (47 - rank) + "位 " + pair[0] + ":" + pair[1].popu10 + "=>" + pair[1].popu15 + " 変化率:" + pair[1].change;
     })
     console.log(rankingString);
 });
-//rl.resume();
+rl.resume();
 
