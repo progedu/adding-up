1f295befae7dddba034c41c18e5008fc606c469b
cnttkhs <cnt.tkhs@gmail.com>
2017-10-21 23:37:14 +0900

    集計年、都道府県、15〜19歳の人口のデータを抜き出す

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ const rs = fs.ReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ 'input': rs, 'output': {} });
 // line イベントが発生したら、読み込んだ1行の文字列が出力される
 rl.on('line', (lineString) => {
-  console.log(lineString);
+  const columns = lineString.split(',');
+  const year = parseInt(columns[0]);
+  const pref = columns[2];
+  const popu = parseInt(columns[7]);
+  if (year === 2010 || year === 2015) {
+    console.log(year);
+    console.log(pref);
+    console.log(popu);
+  }
 });
 // ストリームに情報を流し始める
 rl.resume();
