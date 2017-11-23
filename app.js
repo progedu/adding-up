1c35a934d416e7824db940bdab56cb297d8babd6
ymno <bitplus@outlook.jp>
2017-11-23 00:09:27 +0000

    人口減少割合順ランキング

--- app.js
+++ app.js
@@ -33,13 +33,15 @@ rl.on('close', () => { //無名関数
     for (let pair of map) {//for of 構文でループ処理
         const value = pair[1];//mapの中のvalue（集計データのオブジェクト）を読み込む（pair[0]はkeyの都道府県）
         value.change = value.popu15 / value.popu10; //変化率に2015年の人口を2010年の人口で割ったものを代入
-        //console.log(pair[1]); 集計データのオブジェクトを確認
+        //console.log(pair[1]); //集計データのオブジェクトを確認
     }
     const rankingArray = Array.from(map).sort((pair1, pair2) => {//連想配列から、配列に戻す際に、並び替えを行う
-        return pair2[1].change - pair1[1].change; //並び替えの条件は降順、引き算をして、結果が正であれば、pair2を前に
+        //return pair2[1].change - pair1[1].change; //並び替えの条件は降順、引き算をして、結果が正であれば、pair2を前に
+        return pair1[1].change - pair2[1].change; //並び替えの条件は昇順、引き算をして、結果が正であれば、pair1を前に
     });
-    const rankingStrings = rankingArray.map((pair) => { //map関数で配列の中身に処理を加える
-        return pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
+    const rankingStrings = rankingArray.map((pair, i) => { //map関数で配列の中身に処理を加える
+        //return pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
+        return (i + 1) + '位 ' + pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
     });
     console.log(rankingStrings);
 });
