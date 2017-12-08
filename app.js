3ff32d6e8e8e9a4ab84739e6179ad0c64418f213
apprec100 <apprec100@outlook.com>
2017-12-08 02:58:22 +0000

    データを計算

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ 'input': rs, 'output': {} });
+const map = new Map(); // key: 都道府県 value: 集計データのオブジェクト
 
 // rl オブジェクトで line イベントが発生したら無名関数を呼び、
 // コンソールに引数 lineString の内容を出力
@@ -14,9 +15,30 @@ rl.on('line', (lineString) => {
     const prefecture = columns[2];
     const popu = parseInt(columns[7]);
     if (year === 2010 || year === 2015) {
-        console.log(year);
-        console.log(prefecture);
-        console.log(popu);
+        let value = map.get(prefecture);
+        if (!value) { // その県のデータを処理するのが初めてであれば
+            value = {
+                popu10: 0,
+                popu15: 0,
+                change: null
+            };
+        }
+
+        if (year === 2010) {
+            value.popu10 += popu;
+        }
+        if (year === 2015) {
+            value.popu15 += popu;
+        }
+        map.set(prefecture, value);
     }
 });
 rl.resume();
+
+rl.on('close', () => {
+    for (let pair of map) {
+        const value = pair[1];
+        value.change = value.popu15 / value.popu10;
+    }    
+    console.log(map);
+});
