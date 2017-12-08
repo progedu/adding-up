90fdaad8de39274e0c37f9b90f8585f78403728c
apprec100 <apprec100@outlook.com>
2017-12-08 03:17:00 +0000

    ランキングの実装完了

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ 'input': rs, 'output': {} });
+
 const map = new Map(); // key: 都道府県 value: 集計データのオブジェクト
 
 // rl オブジェクトで line イベントが発生したら無名関数を呼び、
 // コンソールに引数 lineString の内容を出力
 rl.on('line', (lineString) => {
-//    console.log(lineString);
     const columns = lineString.split(',');
     const year = parseInt(columns[0]);
     const prefecture = columns[2];
@@ -39,6 +39,18 @@ rl.on('close', () => {
     for (let pair of map) {
         const value = pair[1];
         value.change = value.popu15 / value.popu10;
-    }    
-    console.log(map);
+    }
+
+    // 変化率の順に並び変え
+    const rankingArray = Array.from(map).sort((pair1, pair2) => {        
+//        return pair1[1].change - pair2[1].change; // 昇順
+        return pair2[1].change - pair1[1].change; // 降順
+    });
+//    console.log(rankingArray);
+    
+    // 整形して表示
+    const rankingStrings = rankingArray.map((pair) => {
+        return pair[0] + ': ' + pair[1].popu10 + ' => ' + pair[1].popu15 + ' 変化率: ' + pair[1].change;
+    });
+    console.log(rankingStrings);    
 });
