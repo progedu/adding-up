40232fd8917696dc5cdcb3bf7c942f98ae9d43af
Akira KAKINOHANA <kira@kirameister.net>
2017-12-16 01:56:10 +0000

    練習問題の解答

--- app.js
+++ app.js
@@ -40,10 +40,10 @@ rl.on('close', () => {
     }
     //const rankingArray = Array.from(map);
     const rankingArray = Array.from(map).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
     });
-    const rankingStrings = rankingArray.map((pair) => {
-        return pair[0] + ':    ' + pair[1].popu10 + ' => ' + pair[1].popu15 + '    変化率: ' + pair[1].change;
+    const rankingStrings = rankingArray.map((pair, index) => {
+        return index+1 + ' -- ' + pair[0] + ':    ' + pair[1].popu10 + ' => ' + pair[1].popu15 + '    変化率: ' + pair[1].change;
     });
     //console.log(rankingArray);
     console.log(rankingStrings);
