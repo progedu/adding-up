d51d04282a9c57d2fee7d80969b866e1d359de23
hasefumi23 <hasefumi23@gmail.com>
2017-12-29 16:24:51 +0900

    Reverse ranking order

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ rl.on('close', () => {
     value.change = value.popu15 / value.popu10;
   }
   const rankingArray = Array.from(map).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
+    return pair1[1].change - pair2[1].change;
   })
   const rankingStrings = rankingArray.map((pair) => {
     return pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
