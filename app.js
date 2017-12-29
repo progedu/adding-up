83acf888f45cd2a79938ebffb49de9e4c15a82e0
hasefumi23 <hasefumi23@gmail.com>
2017-12-29 16:28:49 +0900

    Output the number of rank.

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ rl.on('close', () => {
   const rankingArray = Array.from(map).sort((pair1, pair2) => {
     return pair1[1].change - pair2[1].change;
   })
-  const rankingStrings = rankingArray.map((pair) => {
-    return pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
+  const rankingStrings = rankingArray.map((pair, i) => {
+    return (i + 1) + '位' + pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
   });
   console.log(rankingStrings);
 });
