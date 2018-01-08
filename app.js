e16bbeeae71a3a04e204158cf325974ae31720f1
cjeecsiy <konny198952+github@gmail.com>
2018-01-08 05:46:41 +0000

    Map使った合計出力

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 'use strict';
+const fs = require('fs');
+const readline = require('readline');
+const rs = fs.ReadStream('./popu-pref.csv');
+const rl = readline.createInterface({ 'input': rs, 'output': {} });
+const map = new Map();
+rl.on('line', (lineString) => {
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
+    const prefecture = columns[2];
+    const popu = parseInt(columns[7]);
+    if(year == 2010 || year == 2015){
+        let value = map.get(prefecture);
+        if(!value){
+            value = {
+                popu10: 0,
+                popu15: 0,
+                change: null
+            };
+        }
+        if(year == 2010){
+            value.popu10 += popu;
+        }
+        if(year == 2015){
+            value.popu15 += popu;
+        }
+        map.set(prefecture, value);
+    
+
+    }
+});
+rl.resume();
+rl.on('close', () => {
+    console.log(map);
+})
