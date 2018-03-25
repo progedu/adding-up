fe6eaba15a246e4026143cae8dc64b6857155bc9
ikedaema0817 <maedakei0817@gmail.com>
2018-03-25 02:43:33 +0000

    練習問題

--- app.js
+++ app.js
@@ -1,48 +1,51 @@
-'use strict';
+'use strict'
 const fs = require('fs');
 const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv')
-const rl = readline.createInterface({'input' : rs, 'output' : {}});
+const rl = readline.createInterface({
+  input: rs,
+  output: {}
+});
 const map = new Map();
 
-rl.on('line',(linestring) => {
-    const columns = linestring.split(',');
-    const year = parseInt(columns[0]);
-    const prefecture = columns[2];
-    const popu = parseInt(columns[7]);
-    
-
-    if (year === 2010 || year === 2015) {
-        let value = map.get(prefecture);
-        if (!value) {
-            value = {
-                popu10 : 0,
-                popu15 : 0,
-                change : null
-            };
-        }
-       if (year === 2010) {
-           value.popu10 += popu;
+rl.on ('line', (lineString) => {
+     const column = lineString.split(',');
+     const year = parseInt(column[0]);
+     const prefecture = column[2];
+     const popu = parseInt(column[7]);
+     if (year === 2010 || year === 2015) {
+       let value = map.get(prefecture);
+       if (!value) {
+           value = {
+               popu10: 0,
+               popu15: 0,
+               change: null
+           };
        }
-       if (year === 2015) {
-           value.popu15 += popu;
-       }
-       map.set(prefecture, value);
+    if (year === 2010) {
+        value.popu10 += popu;
+    } 
+    if (year === 2015) {
+        value.popu15 += popu;
     }
+    map.set(prefecture, value);
+    
+  }
 });
 rl.resume();
 rl.on('close', () => {
     for (let pair of map) {
         const value = pair[1];
-        value.change  = value.popu15 / value.popu10;
+        value.change = value.popu15 / value.popu10;
     }
-    const rankingArray = Array.from(map).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+    const rankingArray = Array.from(map).sort((pair4, pair5) => {
+    return pair4[1].change - pair5[1].change;
     });
-    const rankingStrings = rankingArray.map((pair) => {
-        return pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
-    });
-    console.log(rankingStrings);
-});
+    const rankingString = rankingArray.map ((pair, i) => {
+        return (i+1) + '位' + pair[0] + ':' + pair[1].popu10 + '=>' + pair[1].popu15 + '変化率:' + pair[1].change;
+    }
 
-
+    )
+    console.log(rankingString);
+}
+);
