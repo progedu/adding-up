9b370f9373174d11e4c84b5fbff85864b5df4715
Takashi Udagawa <tks.udagawa@gmail.com>
2018-05-17 23:44:49 +0900

    ファイル修正

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const fs = require('fs');
+const readline = require('readline');
+const rs = fs.ReadStream('./popu-pref.csv');
+const rl = readline.createInterface({'input': rs, 'output': {} });
+rl.on('line', (lineString) => {
+    console.log(lineString); 
+});
+rl.resume();
