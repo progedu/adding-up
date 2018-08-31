0a2787602ba66277760a22b7db537f22addd224c
gitloveJefdog <soccer_is_loved_by_me_0507@ezweb.ne.jp>
2018-08-31 14:04:28 +0000

    node.js p246

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 'use strict';
+
+const fs = require('fs');
+const readline = require('readline');
+const rs = fs.ReadStream('./popu-pref.csv');
+const rl = readline.createInterface({'input': rs, 'output': {}});
+const map = new Map();
+rl.on('line', (lineString)=>{
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
+    const prefecture = columns[2];
+    const popu = columns[7];
+    
+    if(year === 2010 || year === 2015){
+        let value = map.get(prefecture);
+    
+        if(value === undefined){
+            value = {
+                popu10: 0,
+                popu15: 0,
+                change: null
+            };
+        }
+       
+        if (year ===2010){
+            value.popu10 += popu;
+        }
+
+        if(year ===2015){
+            value.popu15 +=popu;
+        }
+
+        map.set(prefecture,value);
+    }
+});
+
+rl.resume();
+rl.on('close', ()=>{
+    for(let par of map){
+        const value = par[1];
+       value.change = value.popu15 / value.popu10;
+    }
+   console.log(map);
+});
