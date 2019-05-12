13c3ff464a4f46f867551dd7b19eaa3ea9209216
rozelite0 <rozelite0@gmail.com>
2019-05-12 03:32:52 +0000

    修正

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ rl.on('close', () => {
     const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
         return pair1[1].change - pair2[1].change;
     });
-    const rankingStrings = rankingArray.map(([key, value], j) => {
-        return (j + 1) + '位' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+    const rankingStrings = rankingArray.map(([key, value], i) => {
+        return (i + 1) + '位' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
     });
   console.log(rankingStrings);
 });
