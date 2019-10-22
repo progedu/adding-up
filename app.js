13059a13360f09c352ca5e4f0ff7c80a96fb7e2d
井上葵 <aoi_19n1100650@nnn.ed.jp>
2019-10-22 12:54:09 +0900

    10歳から14歳の人口増加率のランキングを実装し、コンソールに出力されるようにした

--- app.js
+++ app.js
@@ -7,34 +7,47 @@ rl.on('line', (lineString) => {
   const columns = lineString.split(','),
         year = parseInt(columns[0]),
         prefecture = columns[1],
-        popu = parseInt(columns[3]);
+        middler = parseInt(columns[2]),
+        higher = parseInt(columns[3]);
   if (year === 2010 || year === 2015) {
     let value = prefectureDataMap.get(prefecture);
     if (!value) {
       value = {
-        popu10: 0,
-        popu15: 0,
-        change: null
+        popu10: [0, 0],
+        popu15: [0, 0],
+        change: [null, null]
       };
     }
     if (year === 2010) {
-      value.popu10 = popu;
+      value.popu10[0] = middler;
+      value.popu10[1] = higher;
     }
     if (year === 2015) {
-      value.popu15 = popu;
+      value.popu15[0] = middler;
+      value.popu15[1] = higher;
     }
     prefectureDataMap.set(prefecture, value);
   }
 });
 rl.on('close', () => {
   for (let [key, value] of prefectureDataMap) {
-    value.change = value.popu15 / value.popu10;
+    value.change[0] = value.popu15[0] / value.popu10[0];
+    value.change[1] = value.popu15[1] / value.popu10[1];
   }
-  const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
-  });
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return key + ':' + value.popu10 + '=>' + value.popu15 + ' 変化率' + value.change;
-  });
-  console.log(rankingStrings);
+  const middlerRankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+          return pair2[1].change[0] - pair1[1].change[0];
+        }),
+        higherRankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+          return pair2[1].change[1] - pair1[1].change[1];
+        }),
+        middlerRankingStrings = middlerRankingArray.map(([key, value], i) => {
+          return (i + 1) + '位 ' + key + ':' + value.popu10[0] + '=>' + value.popu15[0] + ' 増加率:' + value.change[0];
+        }),
+        higherRankingStrings = higherRankingArray.map(([key, value], i) => {
+          return (i + 1) + '位 ' + key + ':' + value.popu10[1] + '=>' + value.popu15[1] + ' 増加率:' + value.change[1];
+        });
+  console.log('10歳〜14歳の人口増加率ランキング');
+  console.log(middlerRankingStrings);
+  console.log('15歳〜19歳の人口増加率ランキング');
+  console.log(higherRankingStrings);
 });
