0653804423922ce33f7235476626047e7f4da622
Hitoshi Tada <hitoshitada@gaia.eonet.ne.jp>
2019-11-04 19:56:17 +0900

    人口増加率を降順にして順位をつける

--- app.js
+++ app.js
@@ -31,10 +31,10 @@ rl.on('close', () => {
     for (let [key, value] of prefectureDataMap){
         value.change = value.popu15/value.popu10;
     }
-    const rankingArray = Array.from(prefectureDataMap).sort((pair1,pair2) => {return pair2[1].change-pair1[1].change;});
+    const rankingArray = Array.from(prefectureDataMap).sort((pair1,pair2) => {return pair1[1].change-pair2[1].change;});
     console.log(rankingArray);
-    const rankingStrings = rankingArray.map(([key,value]) => {
-        return key +': '+ value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+    const rankingStrings = rankingArray.map(([key,value],oder) => {
+        return (oder+1) +' ;'+key +': '+ value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
     })
     console.log(rankingStrings);
 });
