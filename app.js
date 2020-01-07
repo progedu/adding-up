c28b828a4c97d5aa52a6c9aa30a04b4012e31ca6
Seo Taro <xxxx@seotaro.com>
2020-01-08 00:46:01 +0900

    ランキング表示を昇順にして順位を追記した。

--- app.js
+++ app.js
@@ -35,11 +35,11 @@ rl.on('close', () => {
     }
 
     const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
     });
 
-    const rankingStrings = rankingArray.map(([key, value]) => {
-        return key + ': ' + value.popular10 + '=>' + value.popular15 + ' 変化率:' + value.change;
+    const rankingStrings = rankingArray.map(([key, value], i) => {
+        return (i + 1) + '位 ' + key + ': ' + value.popular10 + '=>' + value.popular15 + ' 変化率:' + value.change;
     })
 
     console.log(rankingStrings);
