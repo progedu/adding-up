5e28a7450ea4ea7a383a4d98ea201562ff4f96ad
channel-4 <ch4.ito.y@gmail.com>
2020-03-08 17:59:58 +0900

    [fix]変化率の低い順のランキングに修正, 先頭に順位を追加

--- app.js
+++ app.js
@@ -42,10 +42,19 @@ rl.on('close', () => {
       value.change = value.popu15 / value.popu10;
     }
     const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-      return pair2[1].change - pair1[1].change;
+      return pair1[1].change - pair2[1].change;
     });
-    const rankingStrings = rankingArray.map(([key, value]) => {
-      return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+    const rankingStrings = rankingArray.map(([key, value], i) => {
+      return (i + 1) + '位 ' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
     });
+
+    const assert = require('assert');
+    assert.equal(
+        rankingStrings[0],
+        '1位 岩手県: 64637=>57619 変化率:0.8914244163559571',
+        '正解は「1位 岩手県: 64637=>57619 変化率:0.8914244163559571」, 実際は「' + rankingStrings[0] + '」'
+    )
+
     console.log(rankingStrings);
-  });
+});
+
