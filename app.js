cd784cbb062b4271852ceed30aee6d3d2494b45f
bnle <1209kousuke@gmail.com>
2020-03-22 03:37:17 +0900

    map関数を使って一括整形適用

--- app.js
+++ app.js
@@ -28,5 +28,14 @@ rl.on('line',(lineString)=>{
     }
 });
 rl.on('close',()=>{
-    console.log(prefectureDataMap);
+    for (let [key,value] of prefectureDataMap){
+        value.change = value.popu15/value.popu10;
+    }
+    const rankingArray = Array.from(prefectureDataMap).sort((pair1,pair2)=>{
+        return pair2[1].change-pair1[1].change;
+    });
+    const rankingStrings = rankingArray.map(([key,value])=>{
+        return key + ': ' + value.popu10 + '=>' + value.popu15 + '変化率:' + value.change;
+    });
+    console.log(rankingStrings);
 })
