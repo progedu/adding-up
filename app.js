00a38eb0e5964587846680b40e3ef566f5dfec76
nakako <nakako.soft@gmail.com>
2020-04-30 17:58:21 +0900

    人が減った割合のランキングに変更

--- app.js
+++ app.js
@@ -51,11 +51,11 @@ rl.on('close', () => {
 
     //データの並び替え
     const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
     });
 
-    const rankingStrings = rankingArray.map(([key, value]) => {
-        return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率：' + value.change;
+    const rankingStrings = rankingArray.map(([key, value], i) => {
+        return (i+1) + '位 ' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率：' + value.change;
     });
 
     //データの表示
