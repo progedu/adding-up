aaad4f25f6bc11c1e02d07666ec6c9e291620fa1
takuya0411 <inu10neko@icloud.com>
2020-05-04 02:59:41 +0900

    都道府県ランキング

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 'use strict';
+const fs = require('fs');
+const readline = require('readline');
+const rs = fs.createReadStream('./popu-pref.csv');
+const rl = readline.createInterface({ 'input': rs, 'output': {} });
+const perfectureDataMap = new Map (); //key: 都道府県 value: 集計データのオブジェクト
+rl.on('line', (lineString) => {
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
+    const perfecture = columns[1];
+    const popu = parseInt(columns[3]);
+    if (year ===2010 || year === 2015) {
+        let value = perfectureDataMap.get(perfecture);
+        if (!value) {
+            value = {
+                popu10: 0,
+                popu15: 0,
+                change: null
+            };
+        }
+        if (year === 2010) {
+            value.popu10 = popu;
+        }
+        if (year === 2015) {
+            value.popu15 = popu;
+        }
+        perfectureDataMap.set(perfecture, value);
+    }
+});
+rl.on('close', () => {
+    for (let [key, value] of perfectureDataMap) {
+        value.change = value.popu15 / value.popu10;
+    }
+    const rankingArray = Array.from(perfectureDataMap).sort((pair1, pair2) => {
+        return pair1[1].change - pair2[1].change;
+    });
+    const rankingStrings = rankingArray.map(([key, value], i) => {
+        return `${i + 1}位 ${key}: ${value.popu10} => ${value.popu15} 変化率: ${value.change}`
+    })
+    console.log(rankingStrings);
+});
