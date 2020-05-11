3a88e9df0412998728ee9963e0c0252c005c1b92
choconanana <luna.asterism375@gmail.com>
2020-05-11 14:40:55 +0900

    順位を出力するよう変更

--- app.js
+++ app.js
@@ -34,8 +34,10 @@ rl.on('close', () => {
     const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
         return pair1[1].change - pair2[1].change;
     });
-    const rankingStrings = rankingArray.map(([key, value]) => {
+    const rankingStrings = rankingArray.map(([key, value], i) => {
         return (
+            (i + 1) +
+            '位 ' +
             key +
             ': ' +
             value.popu10 +
