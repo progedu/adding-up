12ac568a758bb504eb971dddd1cb796bf322dc47
jess285 <67589949+jess285@users.noreply.github.com>
2020-07-26 00:54:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,5 +4,13 @@ const readline = require('readline');
 const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ input: rs, output: {} });
 rl.on('line', lineString => {
-  console.log(lineString);
+  const columns = lineString.split(',');
+  const year = parseInt(columns[0]);
+  const prefecture = columns[1];
+  const popu = parseInt(columns[3]);
+  if (year === 2010 || year === 2015) {
+    console.log(year);
+    console.log(prefecture);
+    console.log(popu);
+  }
 });
