f3ed90a8065ec3f0c341740c1f5b0b3a4659e440
ns336582 <69781177+ns336582@users.noreply.github.com>
2020-09-11 16:14:08 +0900

    練習問題：人口減少率のランキングを実装

--- app.js
+++ app.js
@@ -28,23 +28,16 @@ rl.on('line', lineString => {
   }
 });
 
+
 rl.on('close', () => {
-  for (let [key, value] of prefectureDataMap) {
-    value.change = value.popu15 / value.popu10;
-  }
-  const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
+    for (let [key, value] of prefectureDataMap) {
+      value.change = value.popu15 / value.popu10;
+    }
+    const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+      return pair1[1].change - pair2[1].change;
+    });
+    const rankingStrings = rankingArray.map(([key, value], i) => {
+      return  (i + 1) + '位 ' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+    });
+    console.log(rankingStrings);
   });
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return (
-      key +
-      ': ' +
-      value.popu10 +
-      '=>' +
-      value.popu15 +
-      ' 変化率:' +
-      value.change
-    );
-  });
-  console.log(rankingStrings);
-});
