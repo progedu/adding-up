3b18cc2cd61a641203f7b3797188b9caa283580b
beginnerfuji <teenagefunclub10000@gmail.com>
2020-12-27 05:54:02 +0900

    ランクを昇順に変更、順位を追加

--- app.js
+++ app.js
@@ -38,12 +38,13 @@ rl.on('close',() => {
     for (let [key, value] of prefectureDataMap){
         value.change = value.popu15 / value.popu10;
     }
+    //人口が減っている順番にランキング付けする
     const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
     });
-    const rankingStrings = rankingArray.map(([key, value]) => {
+    const rankingStrings = rankingArray.map(([key, value],i) => {
         return (
-            key + ': ' + value.popu10 + ' => ' + value.popu15 + ' 変化率: ' + value.change
+           i+1 + '位: ' + key + ': ' + value.popu10 + ' => ' + value.popu15 + ' 変化率: ' + value.change
         );
     });
     console.log(rankingStrings);
