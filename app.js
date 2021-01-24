e4463eff180b4990e76867ce1269f1e5a7fc77f3
Jun0422 <juv317life@yahoo.co.jp>
2021-01-24 23:13:17 +0900

    人が 減った割合 のランキングを出力するように修正

--- app.js
+++ app.js
@@ -33,10 +33,13 @@ rl.on("close", () => {
     value.change = value.popu15 / value.popu10;
   }
   const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
+    return pair1[1].change - pair2[1].change;
   });
-  const rankingStrings = rankingArray.map(([key, value]) => {
+  const rankingStrings = rankingArray.map(([key, value], index) => {
     return (
+      index +
+      1 +
+      "位 " +
       key +
       ": " +
       value.popu10 +
