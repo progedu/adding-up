cf7067a2a805d0d40e5ca57fa923794e39c09b65
shota morita <kokoro7791@gmail.com>
2021-02-01 00:06:52 +0900

    ランキング実装完了

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
 'use strict';
 
-const fs = require('fs');
-const readline = require('readline');
-const rs = fs.createReadStream('./popu-pref.csv');
-const rl = readline.createInterface({ input: rs, output: {} });
-rl.on('line', lineString => {
-	const cols = lineString.split(',');
-	const year = parseInt(cols[0]);
-	const prefecture = cols[1];
-	const popu = parseInt(cols[3]);
-	if(year === 2010 || year === 2015){
-		console.log(year);
-		console.log(prefecture);
-		console.log(popu);
-	}
+const fs = require('fs'); // ファイルシステムモジュールのロード
+const readline = require('readline'); // リードライン(1行ずつ読み込む)モジュールのロード
+const rs = fs.createReadStream('./popu-pref.csv'); // .csvをストリーム処理で読み込み、定数rsに渡す
+const rl = readline.createInterface({input: rs, output: {} }); // createInterfaceメソッドに引数rsを渡して、処理結果をrlに渡す
+const preDataMap = new Map(); // key: pre ,value: val
+//lineイベントが発生した時点で、lsをコンソール出力させる
+rl.on('line', ls => {
+  const c = ls.split(',');
+  const y = parseInt(c[0]);
+  const pre = c[1];
+  const pop = parseInt(c[3]);
+  if(y === 2010 || y === 2015){
+    let val = preDataMap.get(pre);
+    if(!val){
+      val = {
+        pop10: 0,
+        pop15: 0,
+        change: null
+      };
+    }
+    if(y === 2010){
+      val.pop10 = pop;
+    }
+    if(y === 2015){
+      val.pop15 = pop;
+    }
+    preDataMap.set(pre, val);
+  }
 });
-
+rl.on('close', ()=>{
+  for(let [key, val] of preDataMap){
+    val.change = val.pop15 / val.pop10;
+  }
+  const r = Array.from(preDataMap).sort((pair1, pair2)=>{
+    return pair2[1].change - pair1[1].change;
+  });
+  const rStr = r.map(([key, val]) => {
+    return (key + ': ' + val.pop10 + '=>' + val.pop15 + ' 変化率:' + val.change);
+  });
+  console.log(rStr);
+});
