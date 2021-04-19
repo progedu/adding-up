cffde6a7b61f0bea5a0972b4eb72f30dc7c23be8
Kiyomi Watanabe <watanabe.kiyomi@piala.co.jp>
2021-04-19 13:51:09 +0900

    逆ランキングの実装完了

--- app.js
+++ app.js
@@ -32,10 +32,12 @@ rl.on('close', () => {
         value.change = value.popu15 / value.popu10;
     }
     const rankingArray = Array.from(prefectureDateMap).sort((pair1, pair2) =>{
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
     });
-    const rankingStrings = rankingArray.map(([key, value]) => {
+    const rankingStrings = rankingArray.map(([key, value], i) => {
+        i = i + 1;
         return(
+            i + '位' +
             key + ': ' +
             value.popu10 + '=>' +
             value.popu15 + ' 変化率:' +
