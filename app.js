aca888318277fe36d189d137270765c19a51ecf6
yohei-nonaka <ynonaka@sayaka.com>
2021-12-08 11:30:42 +0900

    "減った割合"に変更

--- app.js
+++ app.js
@@ -12,17 +12,17 @@ rl.on('line', (lineString) => { // rlオブジェクトでlineイベントが発
     const popu = parseInt(columns[7]);
     if (year === 2010 || year === 2015) {
         let value = map.get(prefecture);
-        if(!value){
+        if (!value) {
             value = {
                 popu10: 0,
                 popu15: 0,
                 change: null
             };
         }
-        if (year === 2010){
+        if (year === 2010) {
             value.popu10 += popu;
         }
-        if (year === 2015){
+        if (year === 2015) {
             value.popu15 += popu;
         }
         map.set(prefecture, value);
@@ -36,10 +36,10 @@ rl.on('close', () => {
     }
     // mapを配列に変換後、sort関数で並び替え
     const rankingArray = Array.from(map).sort((pair1, pair2) => {   // 戻り値が正の時に順番を入れ替える
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
     })
-    const rankingStrings = rankingArray.map((pair) => {
-        return pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
+    const rankingStrings = rankingArray.map((pair, i) => {
+        return (i + 1) + "位:" + pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
     })
     console.log(rankingStrings);
 })
